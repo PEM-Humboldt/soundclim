15c2c821102d50f62e8408a53d6504232dfa24f7
jsulloa <jseb.ulloa@gmail.com>
2020-03-11 11:02:01 -0500

    updated settings managment and stratified sampling

--- 1_compile_dataset.py
+++ 1_compile_dataset.py
@@ -12,61 +12,59 @@ import pandas as pd
 import joblib
 from maad.rois import find_rois_cwt
 from maad import sound
+import settings
 from soundclim_utilities import (features_to_csv, 
                                  batch_find_rois, 
                                  batch_feature_rois, 
                                  listdir_pattern)
 
-## LOAD SETTINGS
-# All settings are stored in the module settings_compile_dataset.py
-from settings import load_compile_dataset
-params_detection, params_features, params_selrois = load_compile_dataset()
-
-
 ## 1. CALIBRATE FIND ROIS
-s, fs = sound.load(params_detection['path_template'])
+s, fs = sound.load(settings.detection['path_template'])
 rois = find_rois_cwt(s, fs, 
-                     flims = params_detection['flims'], 
-                     tlen = params_detection['tlen'], 
-                     th = params_detection['th'], 
+                     flims = settings.detection['flims'], 
+                     tlen = settings.detection['tlen'], 
+                     th = settings.detection['th'], 
                      display=True, figsize=(13,6))
 print(rois)
 
 
 ## 2. BATCH FIND ROIS
-flist = listdir_pattern(params_detection['path_audio'], ends_with='.wav')
+flist = listdir_pattern(settings.detection['path_audio'], ends_with='.wav')
 flist = pd.DataFrame(flist, columns=['fname'])
-detection_data = batch_find_rois(flist, params_detection, 
-                                 params_detection['path_audio'])
-joblib.dump(detection_data, params_detection['path_save'])
+detection_data = batch_find_rois(flist, settings.detection, 
+                                 settings.detection['path_audio'])
+joblib.dump(detection_data, settings.detection['path_save'])
 
 
 ## 3. BATCH FEATURE ROIS
 rois_list = detection_data['detections']
-features = batch_feature_rois(rois_list, params_features, params_features['path_audio'])
-joblib.dump(features, params_features['path_save'])
+features = batch_feature_rois(rois_list, settings.features, settings.features['path_audio'])
+joblib.dump(features, settings.features['path_save'])
 # Write to csv
 df = features_to_csv(features)
-df.to_csv(params_features['path_save'][0:-7]+'.csv', index=False, sep=',')
+df.to_csv(settings.features['path_save'][0:-7]+'.csv', index=False, sep=',')
 
 
 ## 4. SELECT SAMPLES FOR ANNOTATION
 # Filter rois by duration
-tlen_min, tlen_max = params_selrois['tlen_lims']
+tlen_min, tlen_max = settings.selrois['tlen_lims']
 rois_tlen = df.max_t - df.min_t
 idx_keep = (rois_tlen > tlen_min) & (rois_tlen < tlen_max)
 df_sel = df.loc[idx_keep,:]
 
-# Stratified sampling using KMeans
+# Stratified sampling using KMeans. 
 from sklearn.cluster import KMeans
-clf = KMeans(n_clusters=params_selrois['n_clusters'], random_state=42)
+clf = KMeans(n_clusters=settings.selrois['n_clusters'], random_state=42)
 X = df_sel.loc[:,df_sel.columns.str.startswith('shp')]
 clf.fit(X)
 df_sel.loc[:,'cluster'] = clf.labels_
 # stratified sampling 
-df_stratsample = df_sel.groupby('cluster', group_keys=False).apply(lambda x: x.sample(params_selrois['n_samples_per_cluster']))
+n_max = settings.selrois['n_samples_per_cluster']
+gby = df_sel.groupby('cluster', group_keys=False)
+# if cluster is smaller than n_max, take all samples from cluster
+df_stratsample = gby.apply(lambda x: x.sample(n= n_max) if len(x)> n_max else x)
 df_stratsample.reset_index()
-df_stratsample.to_csv(params_selrois['path_save'], index=False)
+df_stratsample.to_csv(settings.selrois['path_save'], index=False)
 
 
 ## 5. VISUALIZE CLUSTERS
--- settings.py
+++ settings.py
@@ -9,29 +9,24 @@ SITE: BETANIA
 @author: jsulloa
 """
 
-# Find ROIs
-def load_compile_dataset():
-    params_detection = {'flims': (1000, 4000),
-                        'tlen': 0.30,
-                        'th': 1e-4,
-                        'path_template': '/media/juan/data/Dropbox/PostDoc/Soundclim/audio_sites/BETANIA/templates/BETA-_20161006_002000_section.wav',
-                        'path_audio': '/media/juan/data/Dropbox/PostDoc/Soundclim/audio_sites/BETANIA/train/',
-                        'path_save': './features/detections_shayii.joblib'}
-    
-    
-    # Feature ROIs
-    params_features = {'flims' : (1000, 4000),
-                       'opt_spec': {'wl': 512, 'ovlp': 0.5, 'db_range': 250},
-                       'opt_shape_str': 'med',
-                       'path_audio': '/media/juan/data/Dropbox/PostDoc/Soundclim/audio_sites/BETANIA/train/',
-                       'path_save': './features/features_med.joblib'}
-    
-    
-    # Select ROIs
-    params_selrois = {'tlen_lims': (0.2, 0.5),
-                      'n_clusters': 12,
-                      'n_samples_per_cluster': 50,
-                      'path_save': './trainds/df_stratsample.csv'}
+detection = {'flims': (1000, 4000),
+             'tlen': 0.30,
+             'th': 1e-4,
+             'path_template': '/media/juan/data/Dropbox/PostDoc/Soundclim/audio_sites/BETANIA/templates/BETA-_20161006_002000_section.wav',
+             'path_audio': '/media/juan/data/Dropbox/PostDoc/Soundclim/audio_sites/BETANIA/train/',
+             'path_save': './features/detections_shayii.joblib'}
+
+
+features = {'flims' : (1000, 4000),
+            'opt_spec': {'wl': 512, 'ovlp': 0.5, 'db_range': 250},
+            'opt_shape_str': 'med',
+            'path_audio': '/media/juan/data/Dropbox/PostDoc/Soundclim/audio_sites/BETANIA/train/',
+            'path_save': './features/features_med.joblib'}
+
+
+selrois = {'tlen_lims': (0.2, 0.5),
+           'n_clusters': 20,
+           'n_samples_per_cluster': 100,
+           'path_save': './trainds/df_stratsample.csv'}
 
     
-    return params_detection, params_features, params_selrois
